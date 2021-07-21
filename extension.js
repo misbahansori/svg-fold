59e6eceea9738f1562b61984677911a695f57145
Misbah Ansori <misbah.ansori24@gmail.com>
2021-07-21 18:40:07 +0700

    Update command and add gif

--- extension.js
+++ extension.js
@@ -12,7 +12,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('svg-collapse.foldAllSVGTag', function () {
+	let disposable = vscode.commands.registerCommand('svg-fold.foldAllSVGTag', function () {
 		// The code you place here will be executed every time your command is executed
 		const document = vscode.window.activeTextEditor.document;
 		const openingSvgRegex = /<svg/g;
