fa8b98a5d6221252eb9b3e60f887d981a8a1890c
Kurkuza <danilsherrbakov1993@gmail.com>
2022-06-21 00:24:20 +0300

    publisher

--- src/extension.js
+++ src/extension.js
@@ -4,7 +4,6 @@ const vscode = require('vscode')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
